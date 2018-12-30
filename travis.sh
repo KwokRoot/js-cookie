d9bfab0233312b407580989d60f1b90e304d453c
Klaus Hartl <klaus.hartl@gmail.com>
2018-12-30 12:45:53 +0100

    Introduce build stages
    This is so we don't run automated browserstack test for each of the
    given nodejs versions, just once.

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Only use `grunt ci` for commits pushed to this repo. Fall back to regular test
-# for pull requests (as secure variables won't be exposed there).
-if [ -z "$BROWSERSTACK_LOCAL_IDENTIFIER" ]; then
-  npm test
-else
-  grunt ci --verbose
-fi
